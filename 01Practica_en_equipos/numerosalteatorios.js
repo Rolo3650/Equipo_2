9befc7ab2f9d324115b8651fae641abbcd5f804c
Eldadmaar <73084680+eldadmaar8@users.noreply.github.com>
2022-04-04 00:19:43 -0500

    Update numerosalteatorios.js

--- 01Practica_en_equipos/numerosalteatorios.js
+++ 01Practica_en_equipos/numerosalteatorios.js
@@ -33,9 +33,9 @@ function validar(formulario){
 }
 
 function calcularMayor(){
-    var n1=document.getElementById("1numero).value;
-    var n2=document.getElementById("2numero).value;
-    var n3=document.getElementById("3numero).value;
+    var n1=document.getElementById("1numero").value;
+    var n2=document.getElementById("2numero").value;
+    var n3=document.getElementById("3numero").value;
     console.log("1er: "+numero1+", 2do: "+numero2+", 3er: "+numero3);
 
     if(numero1<numero2){
