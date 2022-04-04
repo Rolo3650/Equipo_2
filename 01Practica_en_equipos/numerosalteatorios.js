597fdbd9d8bb58f86c7ad5b342172f69a4ab71ac
Eldadmaar <73084680+eldadmaar8@users.noreply.github.com>
2022-04-04 00:43:13 -0500

    Update numerosalteatorios.js

--- 01Practica_en_equipos/numerosalteatorios.js
+++ 01Practica_en_equipos/numerosalteatorios.js
@@ -40,13 +40,13 @@ function calcularMayor(){
 
     if(numero1<numero2){
         if(numero2<numero3){
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3";
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3;
         }else{
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero2";
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero2;
         }
     }else{
         if(numero1<numero3){
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3";
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3;
         }else{
             document.getElementById("resultado").innerHTML="El número mayor es: "+numero1;
         }
