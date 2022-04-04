343f2d0dea5204d542ca5ce8dca9048970d64b59
Eldadmaar <73084680+eldadmaar8@users.noreply.github.com>
2022-04-04 00:21:11 -0500

    Update numerosalteatorios.js

--- 01Practica_en_equipos/numerosalteatorios.js
+++ 01Practica_en_equipos/numerosalteatorios.js
@@ -36,19 +36,19 @@ function calcularMayor(){
     var n1=document.getElementById("1numero").value;
     var n2=document.getElementById("2numero").value;
     var n3=document.getElementById("3numero").value;
-    console.log("1er: "+numero1+", 2do: "+numero2+", 3er: "+numero3);
+    console.log("1er: "+numero1+", 2do: "+numero2+", 3er: "+numero3");
 
     if(numero1<numero2){
         if(numero2<numero3){
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3;
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3";
         }else{
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero2;
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero2";
         }
     }else{
         if(numero1<numero3){
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3;
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero3";
         }else{
-            document.getElementById("resultado").innerHTML="El número mayor es: "+numero1;
+            document.getElementById("resultado").innerHTML="El número mayor es: "+numero1#;
         }
     }
 }
